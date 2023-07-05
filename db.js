98dc0d94be1a371ae64b1d4df65c9b4f7b380289
Akshay Bagai <100753870+Akshaybagai52@users.noreply.github.com>
2023-07-05 16:35:13 +0530

    changed

--- db.js
+++ db.js
@@ -1,2 +1,2 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://yourMongoDBURIGoesHere');
+mongoose.connect('mongodb://akshaybagai52 :akshay@12/fcc-mongodb-and-mongoose?retryWrites=true&w=majority');                   
