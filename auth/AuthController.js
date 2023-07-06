edc34e2b3af0ae581daefc3148418a6eeaac3b97
Akshay Bagai <100753870+Akshaybagai52@users.noreply.github.com>
2023-07-06 11:09:56 +0530

    changed

--- auth/AuthController.js
+++ auth/AuthController.js
@@ -8,27 +8,25 @@ router.use(bodyParser.urlencoded({ extended: false }));
 router.use(bodyParser.json());
 var User = require("../user/User");
 
-router.post("/register", (req, res) => {
-  var hashedPassword = bcrypt.hashSync(req.body.password, 8);
-
-  User.create(
-    {
-      name: req.body.name,
-      email: req.body.email,
-      password: hashedPassword,
+router.post('/register', function(req, res) {
+  
+    var hashedPassword = bcrypt.hashSync(req.body.password, 8);
+    console.log(hashedPassword)
+    
+    User.create({
+      name : req.body.name,
+      email : req.body.email,
+      password : hashedPassword
     },
     function (err, user) {
-      if (err)
-        return res
-          .status(500)
-          .send("There was a problem registering this user");
-      var token = jwt.sign({ id: user_id }, config.secret, {
-        expiresIn: "1h",
+      if (err) return res.status(500).send("There was a problem registering the user.")
+      // create a token
+      var token = jwt.sign({ id: user._id }, config.secret, {
+        expiresIn: 86400 // expires in 24 hours
       });
       res.status(200).send({ auth: true, token: token });
-    }
-  );
-});
+    }); 
+  });
 router.get("/me", function (req, res) {
   var token = req.headers["x-access-token"];
   if (!token)
