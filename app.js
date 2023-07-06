228b39790f3867ddbe7062d15b1fd27ae03ed9b4
Akshay Bagai <100753870+Akshaybagai52@users.noreply.github.com>
2023-07-06 10:27:23 +0530

    AUTHENTICATION

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ var db = require('./db');
 console.log(db.then((res) => {
     return res
 }))
+var AuthController = require('./auth/AuthController');
+app.use('/api/auth', AuthController);
+module.exports = app;
 
 var UserController = require('./user/UserController');
 app.use('/users', UserController);
--- auth/AuthController.js
+++ auth/AuthController.js
@@ -1,9 +1,46 @@
-const express = require('express');
-var jwt = require('jsonwebtoken');
-var bcrypt = require('bcryptjs');
-var config = require('../config');
+const express = require("express");
+var jwt = require("jsonwebtoken");
+var bcrypt = require("bcryptjs");
+var config = require("../config");
 var router = express.Router();
-var bodyParser = require('body-parser');
-router.use(bodyParser.urlencoded({extended: false}))
+var bodyParser = require("body-parser");
+router.use(bodyParser.urlencoded({ extended: false }));
 router.use(bodyParser.json());
-var User = require('../user/User')
+var User = require("../user/User");
+
+router.post("/register", (req, res) => {
+  var hashedPassword = bcrypt.hashSync(req.body.password, 8);
+
+  User.create(
+    {
+      name: req.body.name,
+      email: req.body.email,
+      password: hashedPassword,
+    },
+    function (err, user) {
+      if (err)
+        return res
+          .status(500)
+          .send("There was a problem registering this user");
+      var token = jwt.sign({ id: user_id }, config.secret, {
+        expiresIn: "1h",
+      });
+      res.status(200).send({ auth: true, token: token });
+    }
+  );
+});
+router.get("/me", function (req, res) {
+  var token = req.headers["x-access-token"];
+  if (!token)
+    return res.status(401).send({ auth: false, message: "No token provided." });
+
+  jwt.verify(token, config.secret, function (err, decoded) {
+    if (err)
+      return res
+        .status(500)
+        .send({ auth: false, message: "Failed to authenticate token." });
+
+    res.status(200).send(decoded);
+  });
+});
+module.exports = router;
